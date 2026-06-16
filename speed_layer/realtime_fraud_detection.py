ff02920bdc5a7233a315f6ff22aa6b490aed1ffc
snehasachan4-lab <snehasachan4@gmail.com>
2026-06-16 13:10:48 +0530

    Add files via upload

--- speed_layer/realtime_fraud_detection.py
+++ speed_layer/realtime_fraud_detection.py
@@ -1,1 +1,17 @@
-.
+import pandas as pd
+
+df = pd.read_csv("data/transactions.csv")
+
+fraud_transactions = df[
+df["amount"] > 50000
+]
+
+fraud_transactions.to_csv(
+"data/fraud_alerts.csv",
+index=False
+)
+
+print(
+f"Fraud Transactions Found: {len(fraud_transactions)}"
+)
+
