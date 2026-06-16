f587a5323f27e7b897e971466f3b970e93890cc7
snehasachan4-lab <snehasachan4@gmail.com>
2026-06-16 13:13:00 +0530

    Add files via upload

--- serving_layer/load_to_postgres.py
+++ serving_layer/load_to_postgres.py
@@ -1,1 +1,33 @@
-.
+import pandas as pd
+from sqlalchemy import create_engine
+
+engine = create_engine(
+    "postgresql://postgres:postgres@localhost:5432/frauddb"
+)
+
+customer_df = pd.read_csv("data/customer_metrics.csv")
+merchant_df = pd.read_csv("data/merchant_metrics.csv")
+fraud_df = pd.read_csv("data/fraud_alerts.csv")
+
+customer_df.to_sql(
+    "customer_metrics",
+    engine,
+    if_exists="replace",
+    index=False
+)
+
+merchant_df.to_sql(
+    "merchant_metrics",
+    engine,
+    if_exists="replace",
+    index=False
+)
+
+fraud_df.to_sql(
+    "fraud_alerts",
+    engine,
+    if_exists="replace",
+    index=False
+)
+
+print("Data loaded successfully")
